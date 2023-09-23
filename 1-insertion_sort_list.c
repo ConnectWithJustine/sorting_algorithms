5fb55e1c2730df5fbaf6276b2c4c0ed50e6e4137
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-09-23 14:23:42 +0100

    fix betty error

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,21 +8,20 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *sorted;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	
-	sorted = NULL;   /* Initialize sorted list as empty */
+
+	sorted = NULL;  /* Initialize sorted list as empty */
 	while (*list != NULL)
 	{
 		listint_t *current = *list;  /* Take the first node from the original list*/
 		*list = current->next;    /* Move to the next node in the original list*/
 		if (sorted == NULL || current->n < sorted->n)
 		{
-/**
- * If sorted list is empty or current node's
- * value is smaller, insert it at the beginning 
- * of the sorted list. 
- * */
+/* If sorted list is empty or current node's*/
+ /* value is smaller, insert it at the beginning*/
+ /* of the sorted list.*/
 			current->next = sorted;
 			current->prev = NULL;
 			if (sorted != NULL)
@@ -33,9 +32,10 @@ void insertion_sort_list(listint_t **list)
 		{
 /* Find the correct position in the sorted list and insert it there.*/
 			listint_t *temp = sorted;
+
 			while (temp->next != NULL && temp->next->n < current->n)
 				temp = temp->next;
-			
+
 			current->next = temp->next;
 			current->prev = temp;
 			if (temp->next != NULL)
@@ -43,9 +43,8 @@ void insertion_sort_list(listint_t **list)
 			temp->next = current;
 		}
 
-        /* Print the list after each swap */
+		/* Print the list after each swap */
 		print_list(sorted);
 	}
-	
-	*list = sorted;  /* Update the original list's head to the sorted list*/
+	*list = sorted; /* Update the original list's head to sorted list*/
 }
